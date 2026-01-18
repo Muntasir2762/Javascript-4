a9ff7b1f34d47c21c813f1e3c7d256f65b655e9e
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-01-19 00:25:33 +0600

    for loop

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+// console.log('Hello Wordl!');
+// console.log('Hello Wordl!');
+// console.log('Hello Wordl!');
+// console.log('Hello Wordl!');
+// console.log('Hello Wordl!');
+
+
+// for(let i=1; i<6; i++){ //1 2 3 4 5 6
+//     console.log(i);
+//     console.log('Hello world!');
+// }
+
+let marks = [80, 83, 91, 57, 90, 70, 98, 54, 10, 100];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+// console.log(marks[5]);
+// console.log(marks[6]);
+// console.log(marks[7]);
+// console.log(marks[8]);
+// console.log(marks[9]);
+
+for(let i=0; i<10; i++){ //0, 1 ,2, 3, 4, 5, 6, 7, 8, 9
+    // console.log(marks[i]);
+
+    if(marks[i]<0 || marks[i]>100){
+        console.log('Invalid Marks');
+    }
+    
+    else if(marks[i]>=80){
+        console.log(marks[i]+'=The grade is A+');
+    }
+
+    else if(marks[i]>=70){
+        console.log(marks[i]+'=The grade is A');
+    }
+
+    else if(marks[i]>=60){
+        console.log(marks[i]+'=The grade is A-');
+    }
+
+    else if(marks[i]>=50){
+        console.log(marks[i]+'=The grade is B');
+    }
+
+    else if(marks[i]>=40){
+        console.log(marks[i]+'=The grade is C');
+    }
+
+    else if(marks[i]>=33){
+        console.log(marks[i]+'=The grade is D');
+    }
+
+    else{
+        console.log(marks[i]+'=The grade is F');
+    }
+}
+
